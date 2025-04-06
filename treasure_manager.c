ba1e3d032ea84c2438d4d682e84bb57e3c87f70f
david <david@david.myguest.virtualbox.org>
2025-04-06 10:21:49 +0300

    args completed, need to implement functions now

--- treasure_manager.c
+++ treasure_manager.c
@@ -1,32 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 #include <sys/types.h>
+#include <time.h>
+#include <dirent.h>
+#include <errno.h>
 
-typedef struct GPSCoords_t {
-    float x, y;
-} GPSCoords_t;
+#define USERNAME_MAX 32
+#define CLUE_MAX 256
+#define MAX_PATH 512
 
-typedef struct treasure_t {
-    int ID;
-    char userName[50];
-    GPSCoords_t coords;
-    char clueText[50];
+typedef struct {
+    int treasure_id;
+    char username[USERNAME_MAX];
+    float latitude;
+    float longitude;
+    char clue[CLUE_MAX];
     int value;
-} treasure_t;
+} Treasure;
 
+// Function declarations
+void add_treasure(const char *hunt_id);
+void list_treasures(const char *hunt_id);
+void view_treasure(const char *hunt_id, int treasure_id);
+void remove_treasure(const char *hunt_id, int treasure_id);
+void remove_hunt(const char *hunt_id);
+void log_operation(const char *hunt_id, const char *message);
+void create_symlink(const char *hunt_id);
 
-
-int main(int argc, char** argv) {
-
-    //de pus verificari la argumente
-
-    struct stat st = {0};
-    //char currentDirectory[50] = "/home/student/david";
-
-    if (stat("/home/student/david/treasure_manager", &st) == -1) {
-        mkdir("/home/student/david/treasure_manager", 0700);
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        fprintf(stderr, "Usage:\n"
+                        "--add <hunt_id>\n"
+                        "--list <hunt_id>\n"
+                        "--view <hunt_id><treasure_id>\n"
+                        "--remove_treasure <hunt_id> <treasure_id>\n"
+                        "--remove_hunt <hunt_id>\n");
+        exit(EXIT_FAILURE);
     }
+
+    if (strcmp(argv[1], "--add") == 0) {
+        //add_treasure(argv[2]);
+    } else if (strcmp(argv[1], "--list") == 0) {
+        //list_treasures(argv[2]);
+    } else if (strcmp(argv[1], "--view") == 0) {
+        if (argc < 4) {
+            fprintf(stderr, "Missing treasure ID\n");
+            exit(EXIT_FAILURE);
+        }
+        //view_treasure(argv[2], atoi(argv[3]));
+    } else if (strcmp(argv[1], "--remove_treasure") == 0) {
+        if (argc < 4) {
+            fprintf(stderr, "Missing treasure ID\n");
+            exit(EXIT_FAILURE);
+        }
+        //remove_treasure(argv[2], atoi(argv[3]));
+    } else if (strcmp(argv[1], "--remove_hunt") == 0) {
+        //remove_hunt(argv[2]);
+    } else {
+        fprintf(stderr, "Unknown command\n");
+        exit(EXIT_FAILURE);
+    }
+
     return 0;
 }
